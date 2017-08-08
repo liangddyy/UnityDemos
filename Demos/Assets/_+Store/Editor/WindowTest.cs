90298fb99b9baf35df6fd86b55da985be21129bd
liangddyy <liangddyy@foxmail.com>
2017-08-08 18:04:58 +0800

    0808 18:00

---
+++ Demos/Assets/_+Store/Editor/WindowTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class WindowTest : EditorWindow {
+
+[MenuItem ("GameObject/window")]
+	static void AddWindow(){
+		Rect  rect = new Rect (0,0,500,500);
+        WindowTest window = (WindowTest)EditorWindow.GetWindowWithRect(typeof(WindowTest), rect, true, "测试窗口");
+        window.Show();
+	}
+}
